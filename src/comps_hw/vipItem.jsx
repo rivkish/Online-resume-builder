8eb619057e9f2a9636ece373eb4bed3bdcae0462
rivkish <rivki3232@gmail.com>
2023-12-11 09:56:25 +0200

    main2

--- src/comps_hw/vipItem.jsx
+++ src/comps_hw/vipItem.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const VipItem = (props) => {
     const item=props.item
   return (
-    <div>{item.name}</div>
+    <div className='col-md-6'>
+      <div className='shadow p-2 overflow-hidden h-100'>
+        <img src={item.image} alt={item.name} className="w-25 me-2 float-end" />
+        <h2>{item.name}</h2>
+        <div>Money: {item.worth.toLocaleString()} M USD</div>
+        <div>Company: {item.source}</div>
+      </div>
+    </div>
   )
 }
 
