cb65ce267adfd148c3921671fe26715633764795
rivkish <rivki3232@gmail.com>
2023-12-11 10:26:28 +0200

    main clear

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import AppHw from './comps_hw/appHw';
+
 
 
 function App() {
   return (
 <React.Fragment>
-<AppHw></AppHw>
+
 </React.Fragment>
   );
 }
