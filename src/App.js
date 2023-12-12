d3e69bf6170e033387db4ca7062f096156e480eb
rivkish <rivki3232@gmail.com>
2023-12-12 12:24:18 +0200

    new

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
 import './App.css';
 import React from 'react'
+import Header from "./header";
+import Home from "./comps/home";
+import About from "./comps/about";
+import { Employee } from "./comps/employee";
+import Form from "./comps/form";
+import Counter from "./comps/counter";
 
 
 
 function App() {
   return (
     <BrowserRouter>
-    
+    <Header></Header>
+    <Routes>
+      <Route index element={<Home></Home>}></Route>
+      <Route path="/about" element={<About></About>}></Route>
+      <Route path="/employee" element={<Employee></Employee>}></Route>
+      <Route path="/employee/:company" element={<Employee></Employee>}></Route>
+      <Route path="/form" element={<Form></Form>}></Route>
+      <Route path="/counter" element={<Counter></Counter>}></Route>
+      <Route path="*" element={<h2>404 not found</h2>}></Route>
+    </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+
+
+const Header = () => {
+    return (
+        <header className='p-2 container-fluid bg-dark mb-5  py-3'>
+            <div className='container'>
+                <div className='row align-items-center'>
+<div className='col-auto text-white'>LOGO</div>
+<nav className='nav col-auto'>
+    <ul>
+        <li>  <Link to="/" className='m-2'>Home</Link></li>
+        <li><Link to="/about" className='m-2'>About</Link></li>
+        <li> <Link to="/employee" className='m-2'>Employee</Link></li>
+        <li><Link to="/form" className='m-2'>Form</Link></li>
+        <li><Link to="/counter" className='m-2'>Counter</Link></li>
+        {/* <li> <Link to="/todo" className='m-2'>Todo</Link></li> */}
+    </ul>
+                     
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
