3dab3c22cd00439bfa29963cf2fc4e607a2225f2
rivkish <rivki3232@gmail.com>
2023-12-13 09:26:48 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import './App.css';
 import React from 'react'
 import Header from "./header";
